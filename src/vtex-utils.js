e46d2e22df1a38fd3994a25b72919005859d52b5
Wellington Barreto <zeindelf@hotmail.com>
2018-01-20 03:15:14 -0200

    v0.3.3

--- src/vtex-utils.js
+++ src/vtex-utils.js
@@ -13,7 +13,7 @@ class VtexUtils {
          * Version
          * @type {String}
          */
-        this.version = '0.3.1';
+        this.version = '0.3.3';
 
         /**
          * Package name
